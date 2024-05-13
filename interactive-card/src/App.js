0de0ffb3ca6fcd7ebec7b9ca6cd6e4f3e2ee0187
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-13 14:50:20 -0300

    update app

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -3,15 +3,19 @@ import React from "react";
 export default function App(){
   return <>
     <section>
-      {/* background */}
-      <div>
-        <div></div>
+      background
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div>
         <article>
-          {/* front card*/}
+          front card
         </article>
         <article>
-          {/* back card*/}
+          back card
         </article>
+        </div>
+        <div>
+          form
+        </div>
       </div>
     </section>
   
