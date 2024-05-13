d70960a393f129be3707a7da7336a79ac4cf87cf
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-13 19:17:39 -0300

    styles e logica de preencher o card

--- interactive-card/src/App.js
+++ interactive-card/src/App.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import {format} from "date-fns";
+import InputComponentYup from "../../components/InputComponent/InputComponenteYup";
+import SelectComponentYup from "../../components/InputComponent/SelectComponentYup";
 
 import bgMobile from "./images/bgMobile.png";
 import bgDesktop from "./images/bgDesktop.png";
 import cardFront from "./images/card-front.png";
 import cardBack from "./images/card-back.png";
+import ckeck from "./images/ckeck.png"
 
 import "./index.css";
 
 export default function App(){
-  return <>
+
+  const [confirmed, setConfirmed] = useState(false);
+  const [name, setName] = useState("");
+  const [cardNumber, setCardNumber] = useState("");
+  const [expDate, setExpDate] = useState("01/23");
+  const [cvc, setCVC] = useState("");
+
+  return (<>
     <section>
-      <div className="absolute -z-10">
+      <div className="absolute -z-10 w-full">
         <picture>
           <source media = "(min-width: 1024px)" srcSet={bgDesktop} />
-          <img src={bgMobile} alt=""/>
+          <img src={bgMobile} alt="" className="w-full lg:w-1/3"/>
         </picture>
       </div>
       <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
@@ -21,37 +32,87 @@ export default function App(){
           <article className="front-card p-5 flex flex-col justify-between">
             <div></div>
             <div>
-              <h2 className="text-white text-xl lg:text-3xl mb-1 mt-20 tracking-widest">0000 0000 0000 0000</h2>
+              <h2 className="text-white text-xl lg:text-3xl mb-1 mt-20 tracking-widest">{cardNumber}</h2>
             </div>
             <div>
             <ul className="flex items-center justify-between mb-6">
-                <li className="text-white uppercase tracking-widest">Ana Carolina</li>
-                <li className="text-white tracking-widest">00/00</li>
+                <li className="text-white uppercase tracking-widest">{name}</li>
+                <li className="text-white tracking-widest">{format(new Date(expDate), "MM/yy")}</li>
               </ul>
             </div>
             
           </article>
           <article className="back-card relative">
-            <p className="absolute right-16 text-white tracking-widest">000</p>
+            <p className="absolute right-16 text-white tracking-widest">{cvc}</p>
           </article>
         </div>
 
-        <div>
-          <form>
-            <div>
-              <label htmlFor="name">Nome no cartão</label>
-              <input type="text" name="nome" id="name" placeholder="ex.: Ana Carolina S" required/>
-            </div>
-          </form>
+        <div className="container">
+          {!confirmed && (
+            <>
+            <h1 className="title">Inserir dados do cartão</h1>
+            <form className="flex flex-col justify-center gap-6">
+              <div>
+                <label htmlFor="name">Nome no cartão</label>
+                <input type="text" name="nome" id="name" placeholder="ex.: Ana Carolina S" required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
 
+              </div>
+              <div>
+                <label htmlFor="number">Número do cartão</label>
+                <input type="text" name="nome" id="name" placeholder="ex.: 1234 5678 9123 4567" maxLength={19} required
+                value={cardNumber.replace(/\s/g, "").replace(/(\d{4})/g, "$1 ").trim()}
+                onChange={(e) => setCardNumber(e.target.value)}
+                />
 
+              </div>
+              <article className="flex items-center justify-between gap-6">
+                <div className="flex-1">
+                  <label htmlFor="data_exp">Data de expiração (MM/YY)</label>
+                  <input type="month" name="data_exp" id="data_exp" placeholder="MM YY" required 
+                  value={expDate}
+                  onChange={(e) => setExpDate(e.target.value)}
+                  />
 
+                </div>
+                <div className="flex-1">
+                  <label htmlFor="CVC">CVC</label>
+                  <input type="number" name="CVC" id="CVC" placeholder="123" maxLength={3} required
+                  value={cvc}
+                  onChange={(e) => setCVC(e.target.value)} />
 
+                </div>
+              </article>
+              <button className="btn" onClick={() => setConfirmed(true)}>Confirmar</button>
+            </form></> 
+          )}
+            
+          
+
+          {confirmed && <Obrigado setConfirmed={setConfirmed}/>}
         </div>
       </div>
     </section>
   
   
-  
   </>
+  )
+}
+
+function Obrigado({setConfirmed}){
+  return(
+    <>
+      <div className="container flex flex-col mt-10 mx-auto">
+        <img src={ckeck} alt="" className="block mx-auto"/>
+        <h1 className="title text-3xl mb-6">Obrigado!</h1>
+        <p className="subtitle text-center mb-6">Seu cartão foi adicionado</p>
+        <button className="btn" onClick={() => setConfirmed(false)}>Confirmar</button>
+
+      </div>
+    
+    
+    </>
+  )
 }
