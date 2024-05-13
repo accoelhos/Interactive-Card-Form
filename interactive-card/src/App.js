ea0b0283891e2338f7f3edd5321db6dfa440ad45
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-13 00:16:03 -0300

    create project

---
+++ interactive-card/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function App(){
+  return <>
+    <section>
+      {/* background */}
+      <div>
+        <div></div>
+        <article>
+          {/* front card*/}
+        </article>
+        <article>
+          {/* back card*/}
+        </article>
+      </div>
+    </section>
+  
+  
+  
+  </>
+}
