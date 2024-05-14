7016c183e5ff4bd89c86058d0e738dca98eaf499
Ana Carolina Sá <anacarolina.sacoelho@gmail.com>
2024-05-14 17:49:21 -0300

    styles app

---
+++ interactive-card/src/InputComponent/InputComponenteYup.jsx
@@ -0,0 +1,19 @@
+import "./InputComponent.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+function InputComponentYup(props){
+    const renderError = (message) => <p className="erro">{message}</p>;
+    return (
+        <div className={props.classes}>
+            <label htmlFor={props.id} className="form-label" style={{color:"black"}}>{props.text}</label>
+            <Field
+                name={props.name}
+                type={props.type}
+                id={props.id}
+                placeholder={props.placeholder}
+            />
+            <ErrorMessage name={props.name} render={renderError} />
+        </div>
+    );
+}
+export default InputComponentYup;
